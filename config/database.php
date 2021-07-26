69f06b78471c902a8e4bf8d6bc0a40f82fce7f4b
Alioune Badara Nguirane <badaranguirane120@gmail.com>
2021-07-26 15:09:34 +0000

    database file updated

--- config/database.php
+++ config/database.php
@@ -46,8 +46,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 'db4free.net'),
+            'host' => env('DB_HOST', 'db4free.net'),
+            'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'ecomm'),
             'username' => env('DB_USERNAME', 'aliounedevelopp'),
             'password' => env('DB_PASSWORD', "alioune'sdb"),
